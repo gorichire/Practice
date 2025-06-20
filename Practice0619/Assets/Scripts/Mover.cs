5988801636fb453b700d15c414a8cfe17dddc5ef
gorichire <168408037+gorichire@users.noreply.github.com>
2025-06-20 15:49:01 +0900

    0620

--- Practice0619/Assets/Scripts/Mover.cs
+++ Practice0619/Assets/Scripts/Mover.cs
@@ -16,6 +16,33 @@ public class Mover : MonoBehaviour
     void Update()
     {
         //GetComponent<NavMeshAgent>().destination = target.position;
-        agent.SetDestination(target.position);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+        //Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray , out hit);
+
+        if (hasHit)
+        {
+            target.position = hit.point;
+            agent.SetDestination(target.position);
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
