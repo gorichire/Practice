a015c8fdbb1cdf21bf6484b5c6c7ab58afba0f2c
gorichire <168408037+gorichire@users.noreply.github.com>
2025-07-02 02:01:46 +0900

    0702-1

---
+++ Practice0619/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Saving;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSavingFile = "save";
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            GetComponent<SavingSystem>().Save(defaultSavingFile);
+        }
+
+        private void Load()
+        {
+            GetComponent<SavingSystem>().Load(defaultSavingFile);
+        }
+    }
+}
